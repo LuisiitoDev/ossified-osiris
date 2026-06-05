1faf38bbe5782304d4a0792331b7b37c53ab161f
Luis Alberto Sandoval Monestel <79773016+LuisiitoDev@users.noreply.github.com>
2026-06-05 21:24:47 +0000

    changes

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'LuisiitoDev';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Personal blog by Luis Sandoval covering C#, .NET, Azure, SQL Server, and Software Development.';
